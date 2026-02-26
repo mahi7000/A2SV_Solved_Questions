6d9a4289a3e16904bc33adc8def0c7c9efe49657
Mahlet Belay <136083154+mahi7000@users.noreply.github.com>
2026-02-26 16:07:55 +0300

    Implement strStr function to find first occurrence

---
+++ Find_the_Index_of_the_First_Occurance_in_a_String.py
@@ -0,0 +1,13 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) > len(haystack):
+            return -1
+            
+        for i in range(len(haystack) - len(needle) + 1):
+            j = 0
+            while j < len(needle) and haystack[i + j] == needle[j]:
+                j += 1
+            if j == len(needle):
+                return i
+        
+        return -1
